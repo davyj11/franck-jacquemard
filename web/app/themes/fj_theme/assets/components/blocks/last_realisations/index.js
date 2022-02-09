67f8076d566236f59e2bfeb6b18d60bcf2bd6923
Davy JACQUEMARD <davy.jacquemard@adeliom.com>
2022-02-09 22:43:40 +0100

    BO -FO : dernières réalisations

---
+++ web/app/themes/fj_theme/assets/components/blocks/last_realisations/index.js
@@ -0,0 +1,36 @@
+
+
+console.log('yoo')
+import './styles.pcss'
+
+import Swiper, { Navigation, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+Swiper.use([Navigation, Autoplay]);
+
+
+
+const selectors = {
+    slider: '[js-reals-slider]',
+    container: '[js-reals]',
+    next: '[js-slider-next]',
+    prev: '[js-slider-prev]',
+};
+
+/**
+ * Realisations slider
+ * @type {Swiper}
+ */
+const sliderContainer = document.querySelector(selectors.container);
+
+const realisationsSlider = new Swiper(selectors.slider, {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    navigation: {
+        nextEl: sliderContainer.querySelector(selectors.next),
+        prevEl: sliderContainer.querySelector(selectors.prev),
+    },
+});
+
+console.log(realisationsSlider)
